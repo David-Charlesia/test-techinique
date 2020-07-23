efa94aa974c889991d37fa19bbb706584bc5a39f
David-Charlesia <pro.charlesia.david@outlook.com>
2020-07-23 16:32:37 +0200

    some changes

---
+++ views/view_begin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Students</title>
+	</head>
+	<body>
+		<header>
+			<h1><a href="?"> Students </a></h1>
+		</header>
+
+		<main>
