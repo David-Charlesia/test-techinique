0fd4690fd6d38e440bea02211694ab714cfff5c5
David-Charlesia <pro.charlesia.david@outlook.com>
2020-07-23 16:38:48 +0200

    ...

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php require('views/forms.php'); ?>
+<?php
+require_once "Models/Model.php"; //Inclusion du modèle
+require_once "Controllers/Controller.php"; //Inclusion de la classe Controller
+
+require('views/forms.php');
+
+ ?>
